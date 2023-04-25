b3de42ad01cde367169ea42c41a23937209c0827
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2023-04-25 00:42:03 -0300

    Update addPerfil.py

--- Trabajo final/addPerfil.py
+++ Trabajo final/addPerfil.py
@@ -2,6 +2,8 @@ import PySimpleGUI as sg
 import os
 import json
 
+ruta_imagen = os.path.join(os.getcwd(),"Fotos","usuario.png")
+
 def agregar_perfil():
     if os.path.exists('perfiles.txt'):
         archivo = open("perfiles.txt",'a')
@@ -12,7 +14,8 @@ def agregar_perfil():
           [sg.InputText()],
           [sg.Text('Nombre',font=('Helvetica',10))],
           [sg.InputText()],
-          [sg.Text('Edad',font=('Helvetica',10))],
+          [sg.Image(ruta_imagen,pad=(350,10),key='-AVATAR_IMAGE-',size=(150,100))],
+          [sg.Text('Edad',font=('Helvetica',10)),sg.Button("Seleccionar avatar",pad=(350,10),key='-AVATAR-')],
           [sg.Input()],
           [sg.Text('Genero autopercibido',font=('Helvetica',10))],
           [sg.Combo(['Masculino','Femenino'],default_value='Selecciona una opcion',key='Genero',size=(30,1))], #combo es una lista desplegable
@@ -25,18 +28,24 @@ def agregar_perfil():
 
         if event=="CANCELAR" or event== sg.WINDOW_CLOSED or event == "< Volver":
             break
+        if event == '-AVATAR-':
+            archivo_avatar = sg.popup_get_file('Seleccionar avatar', no_window=True)
+            window['-AVATAR_IMAGE-'].update(filename=archivo_avatar, size=(150, 150))
         if event=='Guardar':
+            print(archivo)
             alias = values[0]
             nombre=values[1]
             edad = values[2]
+            foto = archivo_avatar
             if values['Genero'] == "Selecciona una opcion": #si values['Genero] es igual a "selecciona una opcion" significa que no se eligio ni M ni F entonces busca en la posicion del boton "Otro"
                 genero = values['OtroGenero']
             else: # si es distinto significa que eligio M o F entonces asigna
                 genero = values['Genero']
-            datos = {"Nombre": nombre,"Edad": edad,"Alias":alias,"Genero":genero}
-            json.dump(datos,archivo)
+            datos = {"Nombre": nombre,"Edad": edad,"Alias":alias,"Genero":genero,"Foto":foto}
+            json.dump(datos,archivo,indent= 2)
             archivo.close()            
             break
     window.close()
+
 
 
