b88a80b1cdb3d55a738e6e0d4c1b1de80edfdac2
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2023-04-24 23:39:02 -0300

    Update addPerfil.py

--- Trabajo final/addPerfil.py
+++ Trabajo final/addPerfil.py
@@ -1,23 +1,13 @@
 import PySimpleGUI as sg
+import os
+import json
 
-def mostrar_perfiles(lista):
-    # Definir el diseño de la ventana
-    layout = [[sg.Text("Usuarios")],
-              [sg.Listbox(values=lista, size=(50, len(lista)), key="-LIST-")],
-              [sg.Button("Cerrar")]]
-    
-    # Crear la ventana
-    window = sg.Window("Mostrar lista", layout)
-    while True:
-        event,values = window.read()
-
-        if event == "Cerrar" or event == sg.WINDOW_CLOSED:
-            break
-        
-    window.close()
-
-def agregar_perfil(lista):
-    layout = [[sg.Text('Nuevo Perfil',font=('Helvetica',15)),sg.Button('Volver',pad=(150,50),size=(8,2),button_color=('grey'))],
+def agregar_perfil():
+    if os.path.exists('perfiles.txt'):
+        archivo = open("perfiles.txt",'a')
+    else:
+        archivo = open ("perfiles.txt",'w')
+    layout = [[sg.Text('Nuevo Perfil',font=('Helvetica',15)),sg.Button("< Volver", button_color=('black', 'white'),border_width=0,pad=(250,10))],
           [sg.Text('Nick o alias',font=('Helvetica',10))],
           [sg.InputText()],
           [sg.Text('Nombre',font=('Helvetica',10))],
@@ -27,27 +17,26 @@ def agregar_perfil(lista):
           [sg.Text('Genero autopercibido',font=('Helvetica',10))],
           [sg.Combo(['Masculino','Femenino'],default_value='Selecciona una opcion',key='Genero',size=(30,1))], #combo es una lista desplegable
           [sg.Button("",size=(2,1)),sg.Text("Otro")],
-          [sg.Input("Complete el genero",key = 'Genero')],#arreglar
-          [sg.Button('Guardar',pad=(250,50),size=(8,2),button_color=('sky blue'))]]
+          [sg.Input("Complete el genero",key = 'OtroGenero')],#arreglar
+          [sg.Button('Guardar',pad=(400,10),size=(8,2),button_color=('sky blue'))]]
     window= sg.Window("Crear nuevo perfil",layout,margins=(100,100))
     while True:
         event,values = window.read()
 
-        if event=="CANCELAR" or event== sg.WINDOW_CLOSED or event == 'Volver':
+        if event=="CANCELAR" or event== sg.WINDOW_CLOSED or event == "< Volver":
             break
-        #if event =="Otro":
-         #   genero = values[4]
-        #else:
-         #   genero = values['Genero']
-
         if event=='Guardar':
             alias = values[0]
             nombre=values[1]
             edad = values[2]
-            genero = values['Genero']
-            #print(f"El genero es {genero}")#para verificar
-            lista.append((nombre,edad,alias,genero))
-            #mostrarPerfil(alias,nombre,edad)
+            if values['Genero'] == "Selecciona una opcion": #si values['Genero] es igual a "selecciona una opcion" significa que no se eligio ni M ni F entonces busca en la posicion del boton "Otro"
+                genero = values['OtroGenero']
+            else: # si es distinto significa que eligio M o F entonces asigna
+                genero = values['Genero']
+            datos = {"Nombre": nombre,"Edad": edad,"Alias":alias,"Genero":genero}
+            json.dump(datos,archivo)
+            archivo.close()            
             break
     window.close()
-    return lista
+
+
