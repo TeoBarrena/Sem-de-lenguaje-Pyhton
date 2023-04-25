48f13dce84724fd2429ed22050be476db456719d
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2023-04-25 00:41:44 -0300

    Update inicio.py

--- Trabajo final/inicio.py
+++ Trabajo final/inicio.py
@@ -1,11 +1,10 @@
 import PySimpleGUI as sg
 import crear_perfil as perfil
 
-lista_perfiles=[]
 sg.ChangeLookAndFeel('LightGrey4')
 
 layout = [[sg.Text('UNLPImage', size=(50, 2), justification='left', font=('Helvetica', 15))],
-          [sg.Button('Agregar perfil', size=(20, 2), button_color=('white', 'grey'), font=('Helvetica', 12)), sg.Button('Mostrar datos', size=(20, 2), button_color=('white', 'grey'), font=('Helvetica', 12)), sg.Button('Cerrar', size=(20, 2), button_color=('white', 'grey'), font=('Helvetica', 12))]]
+          [sg.Button('Agregar perfil', size=(20, 2), button_color=('white', 'grey'), font=('Helvetica', 12)),sg.Button('Cerrar', size=(20, 2), button_color=('white', 'grey'), font=('Helvetica', 12))]]
 
 window = sg.Window('', layout, element_justification='c', margins=(150, 200))
 
@@ -15,7 +14,5 @@ while True:
     if event == ('Cerrar') or event == sg.WIN_CLOSED:
         break
     if event == 'Agregar perfil':
-        perfil.agregar_perfil(lista_perfiles)
-    if event == 'Mostrar datos':
-        perfil.mostrar_perfiles(lista_perfiles)
+        perfil.agregar_perfil()
 window.close()
