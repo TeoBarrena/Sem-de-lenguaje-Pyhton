aec3d660e2b16b223ebb760aad1fe288fb650dfd
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2023-06-09 13:16:41 -0800

    Update prueba_streamlit.py

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -1,17 +1,26 @@
 import streamlit as st
-st.header('Mi primera app con Stremalit')
-st.subheader('''Características de st.write
-Muestra texto plano o con formato Markdown''')
-st.write('Hola **mundo**')
-if st.button('Mostrar opciones'):
-    st.write('# Soy un título')
-    st.write(""" * se considera el Swiss Knife de Streamlit. Esto quiere decir␣
-    ↪que tiene la capacidad de renderizar diferentes cosas dependiendo lo que␣
-    ↪reciba como parámetro.
-    """)
-st.subheader('Contenido de variables simples')
-x = 10
-st.write("El valor de x es:", x)
-st.subheader('Contenido de estructuras')
-lista = [1, 2, 3, 4, 5]
-st.write("La lista es:", lista)
+import pandas as pd
+from estadisticas_streamlit import data_set_csv
+from estadisticas_streamlit import data_set_json
+from estadisticas_streamlit import dias_semana
+from estadisticas_streamlit import cantidad_operaciones
+from estadisticas_streamlit import porcentajes_genero
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.header('Logs de UNLP Image')
+
+if st.button("Porcentajes por genero"):
+    #grafico porcentajes
+    st.subheader("Se calculo el porcentaje del genero de los usuarios y en base a esto se hizo el grafico")
+    st.pyplot(porcentajes_genero(data_set_json))
+
+if st.button("Operaciones hechas por dia"):
+
+    #grafico cantidad de operaciones por dia
+    st.subheader("Grafico que compara los dias de la semana en los que se realizaron operaciones")
+    st.pyplot(dias_semana(data_set_csv))
+
+if st.button("Operaciones por usuarios"):
+    #grafico que ordena segun el uso de cada operacion
+    st.subheader("Grafico que compara entre las operaciones hechas por los usuarios")
+    st.pyplot(cantidad_operaciones(data_set_csv))
