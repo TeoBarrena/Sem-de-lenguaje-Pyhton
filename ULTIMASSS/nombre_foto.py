43fb1fb40aad23004ce644b640fa1212413cbf03
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2023-06-17 17:49:53 -0300

    Delete ULTIMASSS directory

--- ULTIMASSS/nombre_foto.py
+++
@@ -1,3 +0,0 @@
-import os
-def name_foto(imagen):
-    return os.path.basename(imagen)
--- ULTIMASSS/unlpimage.py
+++
@@ -1,5 +0,0 @@
-#Este archivo está sujeto a la licencia en el archivo LICENSE.txt.
-#Consulta el archivo de licencia para obtener los términos y condiciones completos.
-import inicio as inicio
-
-inicio.window_inicio()
